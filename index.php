54ca5f97a2e0ced55f9e289258e3a0d16fad9a22
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-31 19:51:42 -0300

    feat: create read

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include_once('./partials/app.php');
+?>
+
+<div class="container mt-2 mb-4">
+    <a class="btn btn-success" href="./system/create.php" role="button">Criar cliente</a>
+</div>
