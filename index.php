853b0c870dad994275fe026f17011e2af4fec2d1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-31 20:02:17 -0300

    change: change directorys

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ include_once('./partials/app.php');
 ?>
 
 <div class="container mt-2 mb-4">
-    <a class="btn btn-success" href="./system/create.php" role="button">Criar cliente</a>
+    <a class="btn btn-success" href="./app/view/create.php" role="button">Criar cliente</a>
 </div>
