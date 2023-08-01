0a7e54dfd93738f139478d25c4ea77c7a9a6774b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-01 09:07:57 -0300

    feat:validations

--- app/view/create.php
+++ app/view/create.php
@@ -4,7 +4,7 @@ include_once('../../partials/app.php');
 ?>
 
 <div class="container mt-2 mb-4">
-    <a class="btn btn-danger" href="../" role="button">Voltar</a>
+    <a class="btn btn-danger" href="../../" role="button">Voltar</a>
 </div>
 
 <div class="outForm">
@@ -12,7 +12,7 @@ include_once('../../partials/app.php');
         <form class="row" method="POST" action="../controller/createController.php">
             <div class="col-md-4">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" placeholder="ex: Igor Marques" name="name">
+                <input type="text" class="form-control" id="name" placeholder="ex: Igor Marques" name="name" value="<?php if (isset($_SESSION['name_input'])) echo $_SESSION['name_input']; unset($_SESSION['name_input']); ?>">
 
                 <?php if (isset($_SESSION['name'])) { ?>
                     <span class="text-danger">
@@ -26,7 +26,7 @@ include_once('../../partials/app.php');
             </div>
             <div class="col-md-4">
                 <label for="email" class="form-label">E-mail</label>
-                <input type="text" class="form-control" id="email" placeholder="ex: fulano@gmail.com" name="email">
+                <input type="text" class="form-control" id="email" placeholder="ex: fulano@gmail.com" name="email" value="<?php if (isset($_SESSION['email_input'])) echo $_SESSION['email_input']; unset($_SESSION['email_input']); ?>">
 
                 <?php if (isset($_SESSION['email'])) { ?>
                     <span class="text-danger">
@@ -40,7 +40,7 @@ include_once('../../partials/app.php');
             </div>
             <div class="col-md-4">
                 <label for="cpf" class="form-label">Cpf</label>
-                <input type="text" class="form-control" id="cpf" placeholder="ex: 000.000.000-00" name="cpf">
+                <input type="text" class="form-control" id="cpf" placeholder="ex: 000.000.000-00" name="cpf" value="<?php if (isset($_SESSION['cpf_input'])) echo $_SESSION['cpf_input']; unset($_SESSION['cpf_input']); ?>">
 
                 <?php if (isset($_SESSION['cpf'])) { ?>
                     <span class="text-danger">
@@ -54,7 +54,7 @@ include_once('../../partials/app.php');
             </div>
             <div class="col-md-6">
                 <label for="telephone" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="telephone" placeholder="ex: 90000-0000" name="telephone">
+                <input type="text" class="form-control" id="telephone" placeholder="ex: 90000-0000" name="telephone" value="<?php if (isset($_SESSION['telephone_input'])) echo $_SESSION['telephone_input']; unset($_SESSION['telephone_input']); ?>">
 
                 <?php if (isset($_SESSION['telephone'])) { ?>
                     <span class="text-danger">
@@ -68,13 +68,13 @@ include_once('../../partials/app.php');
             </div>
             <div class="col-md-6">
                 <label for="birthdate" class="form-label">Data de Nascimento</label>
-                <input type="text" class="form-control" id="birthdate" placeholder="ex: 10/10/2010" name="birthdate">
+                <input type="text" class="form-control" id="birthdate" placeholder="ex: 10/10/2010" name="birthdate" value="<?php if (isset($_SESSION['bithdate_input'])) echo $_SESSION['bithdate_input']; unset($_SESSION['bithdate_input']); ?>">
 
-                <?php if (isset($_SESSION['bithdate'])) { ?>
+                <?php if (isset($_SESSION['birthdate'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['bithdate'];
-                        unset($_SESSION['bithdate']);
+                        echo $_SESSION['birthdate'];
+                        unset($_SESSION['birthdate']);
                         ?>
                     </span>
                 <?php } ?>
