67ed57b8a7279fa6f1aa338504ae7895a08d4631
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-02 10:22:39 -0300

    change: create the news validations

--- app/view/create.php
+++ app/view/create.php
@@ -14,11 +14,10 @@ include_once('../../partials/app.php');
                 <label for="name" class="form-label">Nome</label>
                 <input type="text" class="form-control" id="name" placeholder="ex: Igor Marques" name="name" value="<?php if (isset($_SESSION['name_input'])) echo $_SESSION['name_input']; unset($_SESSION['name_input']); ?>">
 
-                <?php if (isset($_SESSION['name'])) { ?>
+                <?php if (isset($_SESSION['errors']['name'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['name'];
-                        unset($_SESSION['name']);
+                        echo $_SESSION['errors']['name'];
                         ?>
                     </span>
                 <?php } ?>
@@ -28,11 +27,20 @@ include_once('../../partials/app.php');
                 <label for="email" class="form-label">E-mail</label>
                 <input type="text" class="form-control" id="email" placeholder="ex: fulano@gmail.com" name="email" value="<?php if (isset($_SESSION['email_input'])) echo $_SESSION['email_input']; unset($_SESSION['email_input']); ?>">
 
-                <?php if (isset($_SESSION['email'])) { ?>
+                <?php if (isset($_SESSION['errors']['email'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['email'];
-                        unset($_SESSION['email']);
+                        echo $_SESSION['errors']['email'];
+                        unset($_SESSION['errors']['email']);
+                        ?>
+                    </span>
+                <?php } ?>
+
+                <?php if (isset($_SESSION['errors']['email_exist'])) { ?>
+                    <span class="text-danger">
+                        <?php
+                        echo $_SESSION['errors']['email_exist'];
+                        unset($_SESSION['errors']['email_exist']);
                         ?>
                     </span>
                 <?php } ?>
@@ -42,11 +50,20 @@ include_once('../../partials/app.php');
                 <label for="cpf" class="form-label">Cpf</label>
                 <input type="text" class="form-control" id="cpf" placeholder="ex: 000.000.000-00" name="cpf" value="<?php if (isset($_SESSION['cpf_input'])) echo $_SESSION['cpf_input']; unset($_SESSION['cpf_input']); ?>">
 
-                <?php if (isset($_SESSION['cpf'])) { ?>
+                <?php if (isset($_SESSION['errors']['cpf'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['cpf'];
-                        unset($_SESSION['cpf']);
+                        echo $_SESSION['errors']['cpf'];
+                        unset($_SESSION['errors']['cpf']);
+                        ?>
+                    </span>
+                <?php } ?>
+
+                <?php if (isset($_SESSION['errors']['cpf_exist'])) { ?>
+                    <span class="text-danger">
+                        <?php
+                        echo $_SESSION['errors']['cpf_exist'];
+                        unset($_SESSION['errors']['cpf_exist']);
                         ?>
                     </span>
                 <?php } ?>
@@ -56,11 +73,11 @@ include_once('../../partials/app.php');
                 <label for="telephone" class="form-label">Telefone</label>
                 <input type="text" class="form-control" id="telephone" placeholder="ex: 90000-0000" name="telephone" value="<?php if (isset($_SESSION['telephone_input'])) echo $_SESSION['telephone_input']; unset($_SESSION['telephone_input']); ?>">
 
-                <?php if (isset($_SESSION['telephone'])) { ?>
+                <?php if (isset($_SESSION['errors']['telephone'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['telephone'];
-                        unset($_SESSION['telephone']);
+                        echo $_SESSION['errors']['telephone'];
+                        unset($_SESSION['errors']['telephone']);
                         ?>
                     </span>
                 <?php } ?>
@@ -70,11 +87,11 @@ include_once('../../partials/app.php');
                 <label for="birthdate" class="form-label">Data de Nascimento</label>
                 <input type="text" class="form-control" id="birthdate" placeholder="ex: 10/10/2010" name="birthdate" value="<?php if (isset($_SESSION['bithdate_input'])) echo $_SESSION['bithdate_input']; unset($_SESSION['bithdate_input']); ?>">
 
-                <?php if (isset($_SESSION['birthdate'])) { ?>
+                <?php if (isset($_SESSION['errors']['birthdate'])) { ?>
                     <span class="text-danger">
                         <?php
-                        echo $_SESSION['birthdate'];
-                        unset($_SESSION['birthdate']);
+                        echo $_SESSION['errors']['birthdate'];
+                        unset($_SESSION['errors']['birthdate']);
                         ?>
                     </span>
                 <?php } ?>
@@ -86,3 +103,13 @@ include_once('../../partials/app.php');
         </form>
     </div>
 </div>
+
+<script>
+    $('#cpf').mask('000.000.000-00', {
+        reverse: true
+    });
+
+    $('#telephone').mask('(00) 0000-0000');
+
+    $('#birthdate').mask('00/00/0000');
+</script>
