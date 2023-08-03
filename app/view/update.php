13a0704af16a2e5274c6ca236dc14f517fd08791
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-03 14:02:05 -0300

    fix the url, and received the register of DB

--- app/view/update.php
+++ app/view/update.php
@@ -9,7 +9,7 @@ $updateId = update($id);
 ?>
 
 <div class="container mt-2 mb-4">
-    <a class="btn btn-danger" href="../../../" role="button">Voltar</a>
+    <a class="btn btn-danger" href="../../" role="button">Voltar</a>
 </div>
 
 <div class="outForm">
@@ -17,27 +17,27 @@ $updateId = update($id);
         <form class="row" method="POST" action="../controller/updateController.php">
             <div class="col-md-4">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" placeholder="ex: Igor Marques" name="name">
+                <input type="text" class="form-control" id="name" placeholder="ex: Igor Marques" name="name" value="<?= $updateId['name'] ?>">
             </div>
 
             <div class="col-md-4">
                 <label for="email" class="form-label">E-mail</label>
-                <input type="text" class="form-control" id="email" placeholder="ex: fulano@gmail.com" name="email">
+                <input type="text" class="form-control" id="email" placeholder="ex: fulano@gmail.com" name="email" value="<?= $updateId['email'] ?>">
             </div>
 
             <div class="col-md-4">
                 <label for="cpf" class="form-label">Cpf</label>
-                <input type="text" class="form-control" id="cpf" placeholder="ex: 000.000.000-00" name="cpf">
+                <input type="text" class="form-control" id="cpf" placeholder="ex: 000.000.000-00" name="cpf" value="<?= $updateId['cpf'] ?>">
             </div>
 
             <div class="col-md-6">
                 <label for="telephone" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="telephone" placeholder="ex: 90000-0000" name="telephone">
+                <input type="text" class="form-control" id="telephone" placeholder="ex: 90000-0000" name="telephone" value="<?= $updateId['telephone'] ?>">
             </div>
 
             <div class="col-md-6">
                 <label for="birthdate" class="form-label">Data de Nascimento</label>
-                <input type="text" class="form-control" id="birthdate" placeholder="ex: 10/10/2010" name="birthdate">
+                <input type="text" class="form-control" id="birthdate" placeholder="ex: 10/10/2010" name="birthdate" value="<?= $updateId['birthdate'] ?>">
             </div>
 
             <div class="d-grid gap-2 mt-4">
