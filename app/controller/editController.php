7136002d6b9102e19adae9cbf2b53017391079c4
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-04 09:53:28 -0300

    feat: implementation update query

---
+++ app/controller/editController.php
@@ -0,0 +1,33 @@
+<?php
+require_once '../model/DB.php';
+require_once '../repositorys/updateRepository.php';
+
+function edit()
+{
+    global $con;
+
+    $id = filter_input(INPUT_POST, 'id', FILTER_DEFAULT);
+    $name = filter_input(INPUT_POST, 'name', FILTER_DEFAULT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $cpf = filter_input(INPUT_POST, 'cpf', FILTER_DEFAULT);
+    $telephone = filter_input(INPUT_POST, 'telephone', FILTER_DEFAULT);
+    $birthdate = filter_input(INPUT_POST, 'birthdate', FILTER_DEFAULT);
+
+    if (!validate($name, $email, $cpf, $telephone, $birthdate)) {
+        header('Location: ../view/update.php?=' . $id);
+        return;
+    } else {
+        $sql = "UPDATE clientes SET  name = :name, email = :email, cpf = :cpf, telephone = :telephone, birthdate = :birthdate";
+        $update = $con->prepare($sql);
+        $update->bindValue('name', $name);
+        $update->bindValue('email', $email);
+        $update->bindValue('cpf', $cpf);
+        $update->bindValue('telephone', $telephone);
+        $update->bindValue('birthdate', $birthdate);
+        $update->execute();
+
+        header('location: ../../index.php');
+    }
+}
+
+edit();
